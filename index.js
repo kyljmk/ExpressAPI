0121fe88d8632122c6f0eaffb96f385efd4ce490
kyljmk <k.mansonkullin@googlemail.com>
2023-01-17 22:24:29 +0100

    Refactor Validation and Add Delete Function

--- index.js
+++ index.js
@@ -30,30 +30,51 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === Number(req.params.id));
 
-  if (!course) {
-    res.status(404).send("nope");
-  }
+  if (!course) return res.status(404).send("No course with that Id");
   res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const newCourse = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(newCourse);
+  res.status(201).send(newCourse);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === Number(req.params.id));
+  if (!course) return res.status(404).send("No course with that Id");
+
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.status(204).send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === Number(req.params.id));
+  if (!course) {
+    res.status(404).send("No course with that Id");
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.status(204).send(course);
+});
+
+const validateCourse = (course) => {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
   });
 
-  const result = schema.validate(req.body);
-
-  if (result.error) {
-    res.send(result.error.details[0].message);
-  }
-
-  const newCourse = {
-    id: req.body.id,
-    name: req.body.name,
-  };
-
-  res.send(newCourse);
-});
+  return schema.validate(course);
+};
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
