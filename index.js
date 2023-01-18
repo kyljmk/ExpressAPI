12af9f020d9bc2c846544779be14238beb737a29
kyljmk <k.mansonkullin@googlemail.com>
2023-01-18 18:15:51 +0100

    Add Server.js file

--- index.js
+++ index.js
@@ -1,80 +1,20 @@
-const express = require("express");
-const app = express();
-const Joi = require("joi");
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
 
-app.use(express.json());
+const port = process.env.PORT || 8000;
 
-const courses = [
-  {
-    id: 1,
-    name: "javascript",
-  },
-  {
-    id: 2,
-    name: "c#",
-  },
-  {
-    id: 3,
-    name: "ruby",
-  },
-];
-
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === Number(req.params.id));
-
-  if (!course) return res.status(404).send("No course with that Id");
-  res.send(course);
-});
-
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const newCourse = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-
-  courses.push(newCourse);
-  res.status(201).send(newCourse);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === Number(req.params.id));
-  if (!course) return res.status(404).send("No course with that Id");
-
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  course.name = req.body.name;
-  res.status(204).send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === Number(req.params.id));
-  if (!course) {
-    res.status(404).send("No course with that Id");
-  }
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  res.status(204).send(course);
-});
-
-const validateCourse = (course) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  maxPoolSize: 50,
+  writeConcern: 2500,
+  useNewUrlParse: true,
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    console.log(`Listening on port ${port}`);
   });
-
-  return schema.validate(course);
-};
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}..`));
